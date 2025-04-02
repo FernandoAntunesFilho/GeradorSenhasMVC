3d7eaa5d0d35b1ec2b5bfadf77a2228715046517
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-04-02 10:00:11 -0300

    Cria a classe para as constantes básicas para gerar senhas

---
+++ Core/PasswordConstants.cs
@@ -0,0 +1,10 @@
+namespace GeradorSenhasMVC.Core
+{
+    public class PasswordConstants
+    {
+        public const string Minusculas = "abcdefghijklmnopqrstuvwxyz";
+        public const string Maiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string Numeros = "0123456789";
+        public const string Especiais = "!@#$%^&*()-_=+[]{}|;:,.<>?~";
+    }
+}
